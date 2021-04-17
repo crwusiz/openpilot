b58bda827e31739e63f75b90ed97bbc0888308f4
neokii <neokii@gmail.com>
2021-04-17 19:19:24 +0900

    revert

--- selfdrive/controls/lib/latcontrol_lqr.py
+++ selfdrive/controls/lib/latcontrol_lqr.py
@@ -51,7 +51,7 @@ class LatControlLQR():
     lqr_log = log.ControlsState.LateralLQRState.new_message()
 
     steers_max = get_steer_max(CP, CS.vEgo)
-    torque_scale = (0.45 + CS.vEgo / 65.0)**2  # Scale actuator model with speed
+    torque_scale = (0.45 + CS.vEgo / 60.0)**2  # Scale actuator model with speed
 
     # Subtract offset. Zero angle should correspond to zero torque
     steering_angle_no_offset = CS.steeringAngleDeg - params.angleOffsetAverageDeg
