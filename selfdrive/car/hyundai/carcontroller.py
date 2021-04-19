fc477f59ede6ff4647e78fbd16944de49fba78fb
neokii <neokii@gmail.com>
2021-04-19 13:27:02 +0900

    change steer delta down

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -31,7 +31,7 @@ class CarControllerParams:
 class SteerLimitParams:
   STEER_MAX = 384   # 409 is the max, 255 is stock
   STEER_DELTA_UP = 3
-  STEER_DELTA_DOWN = 5
+  STEER_DELTA_DOWN = 4
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
