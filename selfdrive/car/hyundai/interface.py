2c26ebc0add2b4d082bc9cd3e2bc397d24872dfb
neokii <neokii@gmail.com>
2021-05-05 13:59:30 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -67,8 +67,8 @@ class CarInterface(CarInterfaceBase):
     ret.steerMaxV = [1.5]
 
     # longitudinal
-    ret.longitudinalTuning.kpBP = [0., 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1., 0.75, 0.55, 0.42, 0.37, 0.33]
+    ret.longitudinalTuning.kpBP = [0, 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kpV = [0.9, 0.75, 0.55, 0.42, 0.37, 0.33]
     ret.longitudinalTuning.kiBP = [0.]
     ret.longitudinalTuning.kiV = [0.015]
     ret.longitudinalTuning.kf = 0.7
@@ -76,13 +76,13 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.deadzoneV = [0., 0.015]
 
     ret.gasMaxBP = [0., 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
-    ret.gasMaxV = [0.4, 0.26, 0.2, 0.14, 0.11]
+    ret.gasMaxV = [0.42, 0.28, 0.20, 0.14, 0.11]
 
-    ret.brakeMaxBP = [0., 100. * CV.KPH_TO_MS]
-    ret.brakeMaxV = [1.35, 1.]
+    ret.brakeMaxBP = [0., 20. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
+    ret.brakeMaxV = [1., 1.35, 1.]
 
-    ret.stoppingBrakeRate = 0.15  # brake_travel/s while trying to stop
-    ret.startingBrakeRate = 1.2  # brake_travel/s while releasing on restart
+    ret.stoppingBrakeRate = 0.1  # brake_travel/s while trying to stop
+    ret.startingBrakeRate = 1.0  # brake_travel/s while releasing on restart
     ret.startAccel = 1.3
 
     # genesis
