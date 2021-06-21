d9f6adfcb07fc1a10f90cc180fdf2f8447654abd
neokii <neokii@gmail.com>
2021-06-21 21:35:06 +0900

    fix lfa

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -79,6 +79,7 @@ def create_clu11(packer, frame, bus, clu11, button, speed):
 
 def create_lfahda_mfc(packer, enabled, active):
   values = {
+    "HDA_USM": 2,
     "LFA_Icon_State": 2 if enabled else 0,
     "HDA_Active": 1 if active > 0 else 0,
     "HDA_Icon_State": 2 if active > 0 else 0,
