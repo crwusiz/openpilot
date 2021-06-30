5f52cb700cdfc5ac907f6f90e121f5ffeda18107
neokii <neokii@gmail.com>
2021-06-30 23:08:07 +0900

    fix lfa mfc

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -248,14 +248,13 @@ class CarController():
                                       obj_gap, CS.scc14))
       self.scc12_cnt += 1
 
-      # 20 Hz LFA MFA message
-      if frame % 5 == 0:
-        activated_hda = road_speed_limiter_get_active()
-        # activated_hda: 0 - off, 1 - main road, 2 - highway
-        if self.car_fingerprint in FEATURES["send_lfa_mfa"]:
-          state = 1 if self.car_fingerprint in [CAR.K9] else 2
-          can_sends.append(create_lfahda_mfc(self.packer, enabled, activated_hda, state))
-        elif CS.mdps_bus == 0:
-          can_sends.append(create_hda_mfc(self.packer, activated_hda))
+    # 20 Hz LFA MFA message
+    if frame % 5 == 0:
+      activated_hda = road_speed_limiter_get_active()
+      # activated_hda: 0 - off, 1 - main road, 2 - highway
+      if self.car_fingerprint in FEATURES["send_lfa_mfa"]:
+        can_sends.append(create_lfahda_mfc(self.packer, enabled, activated_hda))
+      elif CS.mdps_bus == 0:
+        can_sends.append(create_hda_mfc(self.packer, activated_hda))
 
     return can_sends
