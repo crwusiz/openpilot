a31a78dc0c095d158e43f5c4fd112bfb48cb755e
neokii <neokii@gmail.com>
2021-07-13 11:21:32 +0900

    update sentry.io

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -22,6 +22,6 @@ def bind_extra(**kwargs) -> None:
     sentry_sdk.set_tag(k, v)
 
 def init() -> None:
-  sentry_sdk.init("https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924",
+  sentry_sdk.init("https://e470f9505d6e460ea37a0df9db38db01@o918558.ingest.sentry.io/5861877",
                   default_integrations=False, integrations=[ThreadingIntegration(propagate_hub=True)],
                   release=version)
