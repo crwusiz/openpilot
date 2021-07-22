9bc4991e68eef7e06c9a832d427ad0450cc63a31
neokii <neokii@gmail.com>
2021-07-22 14:53:41 +0900

    merge changes from master-ci

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -70,7 +70,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kpBP = [0, 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     ret.longitudinalTuning.kpV = [1.23, 0.97, 0.83, 0.68, 0.57, 0.48, 0.38]
     ret.longitudinalTuning.kiBP = [0, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.02, 0.015]
+    ret.longitudinalTuning.kiV = [0.03, 0.02]
     ret.longitudinalTuning.kfBP = [10.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     ret.longitudinalTuning.kfV = [1.15, 0.8]
     ret.longitudinalTuning.deadzoneBP = [0., 100.*CV.KPH_TO_MS]
