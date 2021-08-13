3101a1dfaabfeb6f8d8fe4ad6916146bbec43a97
neokii <neokii@gmail.com>
2021-08-13 19:32:53 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -68,11 +68,11 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.2, 0.97, 0.82, 0.72, 0.61, 0.52, 0.45]
+    ret.longitudinalTuning.kpV = [1.2, 0.97, 0.82, 0.735, 0.63, 0.54, 0.46]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.03, 0.02]
     ret.longitudinalTuning.kfBP = [0., 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kfV = [1.0, 0.95]
+    ret.longitudinalTuning.kfV = [1.0, 0.975]
     ret.longitudinalTuning.deadzoneBP = [0., 100.*CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.015]
 
--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -341,7 +341,7 @@ class SccSmoother:
     lead = self.get_lead(sm)
     if lead is not None:
       if not lead.radar:
-        brake_factor *= 0.93
+        brake_factor *= 0.95
 
     if accel > 0:
       accel *= gas_factor
