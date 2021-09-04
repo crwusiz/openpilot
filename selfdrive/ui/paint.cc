0b831784bcbc55741f5dd268ab3c5ee95b2fea1f
neokii <neokii@gmail.com>
2021-09-04 19:35:02 +0900

    cereal

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -548,27 +548,6 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
     bb_ry = bb_y + bb_h;
   }
 
-  float batteryTemp = device_state.getBatteryTempC();
-  bool batteryless =  batteryTemp < -20;
-
-  // add battery level
-    if(UI_FEATURE_RIGHT_BATTERY_LEVEL && !batteryless) {
-    char val_str[16];
-    char uom_str[6];
-    char bat_lvl[4] = "";
-    NVGcolor val_color = nvgRGBA(255, 255, 255, 200);
-
-    int batteryPercent = device_state.getBatteryPercent();
-
-    snprintf(val_str, sizeof(val_str), "%d%%", batteryPercent);
-    snprintf(uom_str, sizeof(uom_str), "");
-    bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "BAT LVL",
-        bb_rx, bb_ry, bb_uom_dx,
-        val_color, lab_color, uom_color,
-        value_fontSize, label_fontSize, uom_fontSize );
-    bb_ry = bb_y + bb_h;
-  }
-
   // add panda GPS altitude
   if (UI_FEATURE_RIGHT_GPS_ALTITUDE) {
     char val_str[16];
