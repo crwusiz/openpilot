3abfbb35b60d5632984d9de2b7ddea5f7c518ca3
neokii <neokii@gmail.com>
2021-10-25 17:10:06 +0900

    update merge animation speed

--- selfdrive/ui/extras.h
+++ selfdrive/ui/extras.h
@@ -4,6 +4,68 @@
 #include <sys/stat.h>
 #include <cmath>
 
+#pragma once
+
+#include <vector>
+
+class ATextItem {
+  public:
+    std::string text;
+    int alpha;
+
+    ATextItem(const char* text, int alpha) {
+      this->text = text;
+      this->alpha = alpha;
+    }
+};
+
+class AText {
+  private:
+    NVGcolor color;
+    std::string font_name;
+
+    std::vector<ATextItem> after_items;
+    std::string last_text;
+
+  public:
+
+    AText(NVGcolor color, const char *font_name) {
+      this->color = color;
+      this->font_name = font_name;
+    }
+
+    void update(const UIState *s, float x, float y, const char *string, int size) {
+      if(last_text != string) {
+        after_items.insert(after_items.begin(), ATextItem(string, 255));
+        last_text = string;
+      }
+
+      for(auto it = after_items.begin() + 1; it != after_items.end();)  {
+        it->alpha -= 70;
+        if(it->alpha <= 0)
+          it = after_items.erase(it);
+        else
+          it++;
+      }
+
+      nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
+
+      for(auto it = after_items.rbegin(); it != after_items.rend(); ++it)  {
+        NVGcolor color = this->color;
+        color.a = it->alpha/255.f;
+        ui_draw_text(s, x, y, it->text.c_str(), size, color, this->font_name.c_str());
+      }
+    }
+
+    void ui_draw_text(const UIState *s, float x, float y, const char *string, float size, NVGcolor color, const char *font_name) {
+      nvgFontFace(s->vg, font_name);
+      nvgFontSize(s->vg, size);
+      nvgFillColor(s->vg, color);
+      nvgText(s->vg, x, y, string, NULL);
+    }
+};
+
+
 static void ui_draw_extras_limit_speed(UIState *s) {
     const UIScene *scene = &s->scene;
     cereal::CarControl::SccSmoother::Reader scc_smoother = scene->car_control.getSccSmoother();
@@ -11,7 +73,7 @@ static void ui_draw_extras_limit_speed(UIState *s) {
     int limit_speed = scc_smoother.getRoadLimitSpeed();
     int left_dist = scc_smoother.getRoadLimitSpeedLeftDist();
 
-    if (activeNDA > 0) {
+    if(activeNDA > 0) {
         int w = 120;
         int h = 54;
         int x = (s->fb_w + (bdr_s*2))/2 - w/2 - bdr_s;
@@ -21,7 +83,7 @@ static void ui_draw_extras_limit_speed(UIState *s) {
         ui_draw_image(s, {x, y, w, h}, img, 1.f);
     }
 
-    if (limit_speed > 10 && left_dist > 0) {
+    if(limit_speed > 10 && left_dist > 0) {
         int w = s->fb_w / 10;
         int h = s->fb_w / 10;
         int x = (bdr_s*2) + 200 + s->fb_w / 25;
