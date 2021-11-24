a3502673c154158add0f3540ad4484089b18301e
neokii <neokii@gmail.com>
2021-11-24 23:14:35 +0900

    merge changes

--- selfdrive/shutdownd.py
+++ selfdrive/shutdownd.py
@@ -7,14 +7,14 @@ import cereal.messaging as messaging
 
 def main():
 
-  shutdown_at = 60 * 10
+  shutdown_at = 60
   shutdown_count = 0
   device_state_sock = messaging.sub_sock('deviceState')
 
   while 1:
     msg = messaging.recv_sock(device_state_sock, wait=True)
     if msg is not None:
-      if not msg.deviceState.started and not msg.deviceState.usbOnline:
+      if not msg.deviceState.usbOnline:
         shutdown_count += 5
       else:
         shutdown_count = 0
