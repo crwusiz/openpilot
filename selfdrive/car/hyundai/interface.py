5db51bf84aa95d5789c46c40396c5497aeffcec3
neokii <neokii@gmail.com>
2021-12-28 09:32:07 +0900

    lat tuning

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -232,15 +232,15 @@ class CarInterface(CarInterfaceBase):
           ret.lateralTuning.lqr.k = [-110.0, 451.0]
           ret.lateralTuning.lqr.l = [0.33, 0.318]
       elif candidate in [CAR.GRANDEUR, CAR.GRANDEUR_HEV, CAR.GRANDEUR20, CAR.GRANDEUR20_HEV, CAR.K7, CAR.K7_HEV]:
-          ret.lateralTuning.init('lqr')
-          ret.lateralTuning.lqr.scale = 1700.
-          ret.lateralTuning.lqr.ki = 0.01
-          ret.lateralTuning.lqr.dcGain = 0.0028
-          ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-          ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-          ret.lateralTuning.lqr.c = [1., 0.]
-          ret.lateralTuning.lqr.k = [-110., 451.]
-          ret.lateralTuning.lqr.l = [0.33, 0.318]
+        ret.lateralTuning.init('lqr')
+        ret.lateralTuning.lqr.scale = 1550.
+        ret.lateralTuning.lqr.ki = 0.01
+        ret.lateralTuning.lqr.dcGain = 0.0027
+        ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+        ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+        ret.lateralTuning.lqr.c = [1., 0.]
+        ret.lateralTuning.lqr.k = [-110., 451.]
+        ret.lateralTuning.lqr.l = [0.33, 0.318]
       elif candidate == CAR.SELTOS:
           ret.lateralTuning.init('lqr')
           ret.lateralTuning.lqr.scale = 1500.0
@@ -300,11 +300,11 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kiV = [0.13, 0.08]
 
     # steer, gas, brake limitations VS speed
-    ret.steerActuatorDelay = 0.05
+    ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 2.5
-    ret.steerRateCost = 0.4
+    ret.steerRateCost = 0.35
     ret.steerMaxBP = [0.]
-    ret.steerMaxV = [1.5]
+    ret.steerMaxV = [2.]
 
     ret.startAccel = -0.8
     ret.stopAccel = -2.0
