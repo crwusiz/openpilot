cf18c20853966d2424b975099f8ab8aaa61e8236
neokii <neokii@gmail.com>
2022-01-07 00:05:14 +0900

    merge changes

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -430,7 +430,7 @@ class Controls:
     # if stock cruise is completely disabled, then we can use our own set speed logic
     #if not self.CP.pcmCruise:
     #  self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.button_timers, self.enabled, self.is_metric)
-    #elif self.CP.pcmCruise and CS.cruiseState.enabled:
+    #elif CS.cruiseState.enabled:
     #  self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
 
     SccSmoother.update_cruise_buttons(self, CS, self.CP.openpilotLongitudinalControl)
