4013222612b32cda0e3e6b9cd3e268db465eddd9
ZwX1616 <zwx1616@gmail.com>
2022-06-20 16:24:43 -0700

    fullframe DM model (#24860)
    * Revert "put cereal on master"
    
    This reverts commit a8ccd8f838aff958a60546de04bf42220567b288.
    
    * Revert "Revert fullframe DM model (#24812)"
    
    This reverts commit c646eeee0ac54925db5afc51b95c5d869d6dba68.
    
    * revert revert cereal
    
    * clip6
    
    * 0.8 is fair
    
    * Fiction compensation should be based on error
    
    * Update refs
    
    * Add deadzone
    
    * not that
    
    * good mg
    
    * ref
    
    * ref
    
    * ee8f
    
    * minor tweak
    
    * ref
    
    * recompile
    
    * ref
    
    * cereal
    
    * match driverstatus
    
    * new ref
    
    * new ref
    
    * pass token through jenkins credentials
    
    * quote
    
    * fix snpe dead weights
    
    * final ref
    
    Co-authored-by: Harald Schafer <harald.the.engineer@gmail.com>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/ui/qt/offroad/driverview.cc
+++ selfdrive/ui/qt/offroad/driverview.cc
@@ -53,8 +53,7 @@ void DriverViewScene::paintEvent(QPaintEvent* event) {
     p.setPen(Qt::white);
     p.setRenderHint(QPainter::TextAntialiasing);
     configFont(p, "Inter", 100, "Bold");
-    //p.drawText(geometry(), Qt::AlignCenter, "camera starting");
-    p.drawText(geometry(), Qt::AlignCenter, "카메라 준비중");
+    p.drawText(geometry(), Qt::AlignCenter, "camera starting");
     return;
   }
 
@@ -64,7 +63,7 @@ void DriverViewScene::paintEvent(QPaintEvent* event) {
   // is_rhd = driver_state.getWheelOnRightProb() > 0.5;
   driver_data = is_rhd ? driver_state.getRightDriverData() : driver_state.getLeftDriverData();
 
-  bool face_detected = driver_data.getFaceProb() > 0.5;
+  bool face_detected = driver_data.getFaceProb() > 0.7;
   if (face_detected) {
     auto fxy_list = driver_data.getFacePosition();
     auto std_list = driver_data.getFaceOrientationStd();
