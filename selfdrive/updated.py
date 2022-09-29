29ce4f599fea0a1e17d6afbbe71ded3d747ce964
neokii <neokii@gmail.com>
2022-09-29 11:07:05 +0900

    merge changes

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -54,8 +54,8 @@ FINALIZED = os.path.join(STAGING_ROOT, "finalized")
 
 OVERLAY_INIT = Path(os.path.join(BASEDIR, ".overlay_init"))
 
-DAYS_NO_CONNECTIVITY_MAX = 14     # do not allow to engage after this many days
-DAYS_NO_CONNECTIVITY_PROMPT = 10  # send an offroad prompt after this many days
+DAYS_NO_CONNECTIVITY_MAX = 365     # do not allow to engage after this many days
+DAYS_NO_CONNECTIVITY_PROMPT = 365  # send an offroad prompt after this many days
 
 class WaitTimeHelper:
   def __init__(self):
