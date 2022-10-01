182829fc1bcf9125fd7121ee1f48bc2d13f93e71
Cameron Clough <cameronjclough@gmail.com>
2022-10-02 06:47:06 +0900

    networking: add unmetered cellular toggle (#25902)
    * add metered toggle to UI
    
    * add GsmMetered param
    
    * add NMMetered constants
    
    * change LTE connection settings: connection.metered
    
    * change to GsmUnmetered override
    
    * update translations
    
    * debug ui
    
    * remove comment
    
    * Revert "debug ui"
    
    This reverts commit 2ad9e65ea229b814782be9f30cc7664125d7e908.
    
    * 'Force Unmetered Cellular' toggle
    
    * update translations
    
    * remove description
    
    * update translations
    
    * change unmetered to metered
    
    (cherry picked from commit f6119603912efa4fe28f5da21f3d57f6903a0c77)

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -38,6 +38,7 @@ def manager_init() -> None:
   default_params: List[Tuple[str, Union[str, bytes]]] = [
     ("CompletedTrainingVersion", "0"),
     ("DisengageOnAccelerator", "0"),
+    ("GsmMetered", "1"),
     ("HasAcceptedTerms", "0"),
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
