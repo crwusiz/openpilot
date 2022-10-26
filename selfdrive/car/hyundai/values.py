b24a2b9a5907e9fa9701344003e8da82719efd86
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2022-10-27 03:26:41 +0900

    Hyundai: Add FW for 2022 Tucson Hybrid (#26239)
    * Hyundai: Add FW for 2022 Tucson Hybrid
    
    * Duplicated FW
    
    * Add cornerRadar FW (in a non-HDA too?)
    
    * Update selfdrive/car/hyundai/values.py
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    
    (cherry picked from commit b3e0a8c4e05f0c657d63d4825d50d43877b72768)

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1436,6 +1436,7 @@ FW_VERSIONS = {
   CAR.TUCSON22_HEV: {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9240 14Q',
+      b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9220 14K',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00NX4__               1.00 1.00 99110-N9100         ',
