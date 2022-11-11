962f7006656148f5c8676be59c5d9c7956056d02
Shane Smiskol <shane@smiskol.com>
2022-11-12 07:43:35 +0900

    Hyundai: fix button enable controls mismatch (#1152)
    * fix hyundai long button controls mismatch
    
    * test
    
    * comment
    
    (cherry picked from commit d57311126860c9a87edf5b7b9f81a2a038866a26)

--- panda/board/safety/safety_hyundai_common.h
+++ panda/board/safety/safety_hyundai_common.h
@@ -47,10 +47,9 @@ void hyundai_common_cruise_state_check(const int cruise_engaged) {
       controls_allowed = 1;
     }
 
-    /*if (!cruise_engaged) {
+    if (!cruise_engaged) {
       controls_allowed = 0;
-    }*/
-    controls_allowed = 1;
+    }
     cruise_engaged_prev = cruise_engaged;
   }
 }
@@ -64,16 +63,16 @@ void hyundai_common_cruise_buttons_check(const int cruise_button, const int main
   }
 
   if (hyundai_longitudinal) {
-    // exit controls on cancel press
-    /*if (cruise_button == HYUNDAI_BTN_CANCEL) {
-      controls_allowed = 0;
-    }*/
-
     // enter controls on falling edge of resume or set
-    bool set = (cruise_button == HYUNDAI_BTN_NONE) && (cruise_button_prev == HYUNDAI_BTN_SET);
-    bool res = (cruise_button == HYUNDAI_BTN_NONE) && (cruise_button_prev == HYUNDAI_BTN_RESUME);
+    bool set = (cruise_button != HYUNDAI_BTN_SET) && (cruise_button_prev == HYUNDAI_BTN_SET);
+    bool res = (cruise_button != HYUNDAI_BTN_RESUME) && (cruise_button_prev == HYUNDAI_BTN_RESUME);
     if (set || res) {
       controls_allowed = 1;
+    }
+
+    // exit controls on cancel press
+    if (cruise_button == HYUNDAI_BTN_CANCEL) {
+      controls_allowed = 0;
     }
 
     cruise_button_prev = cruise_button;
