a2c8ec2bd5c2f166f4077dd64235f557a4569a05
Shane Smiskol <shane@smiskol.com>
2022-11-16 09:44:03 +0900

    offroad ui: support storing confirmation of a toggle (#26510)
    * show confirmation toggle on first toggle of experimental mode
    
    * don't store confirmation if users toggle off *after* this PR
    
    * refactor
    
    * cleaner
    
    * not true
    
    * try here
    
    (cherry picked from commit 2ad9a4f95a82bf922c86b373026c0e96f9971a80)
    
    fix

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -35,20 +35,18 @@
 
 TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
   // param, title, desc, icon, confirm
-  std::vector<std::tuple<QString, QString, QString, QString, bool>> toggle_defs{
+  std::vector<std::tuple<QString, QString, QString, QString>> toggle_defs{
     {
       "OpenpilotEnabledToggle",
       tr("Enable openpilot"),
       tr("Use the openpilot system for adaptive cruise control and lane keep driver assistance. Your attention is required at all times to use this feature. Changing this setting takes effect when the car is powered off."),
       "../assets/offroad/icon_openpilot.png",
-      false,
     },
     {
       "ExperimentalMode",
       tr("Experimental Mode"),
       "",
       "../assets/img_experimental.png",
-      false,
     },
     {
       "ExperimentalLongitudinalEnabled",
@@ -57,42 +55,36 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       .arg(tr("WARNING: openpilot longitudinal control is experimental for this car and will disable Automatic Emergency Braking (AEB)."))
       .arg(tr("openpilot defaults to the car's built-in ACC instead of openpilot's longitudinal control on this car. Enable this to switch to openpilot longitudinal control.")),
       "../assets/offroad/icon_speed_limit.png",
-      true,
     },
     {
       "IsLdwEnabled",
       tr("Enable Lane Departure Warnings"),
       tr("Receive alerts to steer back into the lane when your vehicle drifts over a detected lane line without a turn signal activated while driving over 31 mph (50 km/h)."),
       "../assets/offroad/icon_ldws.png",
-      false,
     },
     {
       "AutoLaneChangeEnabled",
       tr("Enable AutoLaneChange"),
       tr("Operation of the turn signal at 60㎞/h speed will result in a short change of the vehicle"),
       "../assets/offroad/icon_lca.png",
-      false,
     },
     {
       "IsMetric",
       tr("Use Metric System"),
       tr("Display speed in km/h instead of mph."),
       "../assets/offroad/icon_metric.png",
-      false,
     },
     {
       "RecordFront",
       tr("Record and Upload Driver Camera"),
       tr("Upload data from the driver facing camera and help improve the driver monitoring algorithm."),
       "../assets/offroad/icon_monitoring.png",
-      false,
     },
     {
       "DisengageOnAccelerator",
       tr("Disengage on Accelerator Pedal"),
       tr("When enabled, pressing the accelerator pedal will disengage openpilot."),
       "../assets/offroad/icon_disengage_on_accelerator.svg",
-      false,
     },
 #ifdef ENABLE_MAPS
     {
@@ -100,26 +92,29 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       tr("Show ETA in 24h Format"),
       tr("Use 24h format instead of am/pm"),
       "../assets/offroad/icon_metric.png",
-      false,
     },
     {
       "NavSettingLeftSide",
       tr("Show Map on Left Side of UI"),
       tr("Show map on left side when in split screen view."),
-      "../assets/offroad/icon_map.png",
-      false,
+      "../assets/offroad/icon_road.png",
     },
 #endif
   };
 
-  for (auto &[param, title, desc, icon, confirm] : toggle_defs) {
-    auto toggle = new ParamControl(param, title, desc, icon, confirm, this);
+  for (auto &[param, title, desc, icon] : toggle_defs) {
+    auto toggle = new ParamControl(param, title, desc, icon, false, this);
 
     bool locked = params.getBool((param + "Lock").toStdString());
     toggle->setEnabled(!locked);
     addItem(toggle);
     toggles[param.toStdString()] = toggle;
   }
+
+  // Toggles with confirmation dialogs
+  toggles["ExperimentalMode"]->confirm = true;
+  toggles["ExperimentalMode"]->store_confirm = true;
+  toggles["ExperimentalLongitudinalEnabled"]->confirm = true;
 
   connect(toggles["ExperimentalLongitudinalEnabled"], &ToggleControl::toggleFlipped, [=]() {
     updateToggles();
