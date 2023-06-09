86a271686cc84da38ad0b7225a8ca7c90484dde1
crwusiz <crwusiz@gmail.com>
2023-06-09 16:29:18 +0900

    kor translation update

--- selfdrive/ui/translations/main_ko.ts
+++ selfdrive/ui/translations/main_ko.ts
@@ -1282,6 +1282,26 @@ This may take up to a minute.</source>
         <source>On this car, openpilot defaults to the car&apos;s built-in ACC instead of openpilot&apos;s longitudinal control. Enable this to switch to openpilot longitudinal control. Enabling Experimental mode is recommended when enabling openpilot longitudinal control alpha.</source>
         <translation>이 차량은 openpilot 롱컨트롤 대신 차량의 내장 ACC로 기본 설정됩니다. openpilot 롱컨트롤으로 전환하려면 이 기능을 활성화하세요. openpilot 롱컨트롤 알파를 활성화하는경우 실험적 모드 활성화를 권장합니다.</translation>
     </message>
+    <message>
+        <source>Aggressive</source>
+        <translation>공격적</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>표준</translation>
+    </message>
+    <message>
+        <source>Relaxed</source>
+        <translation>편안한</translation>
+    </message>
+    <message>
+        <source>Driving Personality</source>
+        <translation>주행 모드</translation>
+    </message>
+    <message>
+        <source>Standard is recommended. In aggressive mode, openpilot will follow lead cars closer and be more aggressive with the gas and brake. In relaxed mode openpilot will stay further away from lead cars.</source>
+        <translation>표준 모드를 권장합니다. 공격적 모드에서는 openpilot은 앞차를 더 가까이 따라가며 가속과 감속을 더 공격적으로 사용합니다. 편안한 모드에서 openpilot은 선두 차량에서 더 멀리 떨어져 있습니다.</translation>
+    </message>
 </context>
 <context>
     <name>Updater</name>
