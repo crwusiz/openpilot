fca4e5c68252f7fa9e89da17ebeaa351a9d9882d
crwusiz <crwusiz@gmail.com>
2023-08-03 20:16:49 +0900

    update master-ci

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -66,7 +66,7 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.75
+    return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
     return 0.5
   else:
