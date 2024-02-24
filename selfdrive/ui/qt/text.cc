c17f3757e75d9aed9e59d4bdfc2ccc5467fa40e3
crwusiz <crwusiz@gmail.com>
2024-02-24 20:11:13 +0900

    update master

--- selfdrive/ui/qt/text.cc
+++ selfdrive/ui/qt/text.cc
@@ -4,10 +4,6 @@
 #include <QScrollBar>
 #include <QVBoxLayout>
 #include <QWidget>
-#include <QProcess>
-#include <QHostAddress>
-#include <QNetworkInterface>
-#include <QAbstractSocket>
 
 #include "system/hardware/hw.h"
 #include "selfdrive/ui/qt/util.h"
@@ -37,16 +33,6 @@ int main(int argc, char *argv[]) {
 
   QPushButton *btn = new QPushButton();
 #ifdef __aarch64__
-  QLabel *label2 = new QLabel();
-  QString device_ip = "────────";
-  const QHostAddress &localhost = QHostAddress(QHostAddress::LocalHost);
-  for (const QHostAddress &address: QNetworkInterface::allAddresses()) {
-    if (address.protocol() == QAbstractSocket::IPv4Protocol && address != localhost)
-      device_ip = address.toString();
-  }
-  label2->setText(device_ip);
-  label2->setStyleSheet("color: #e0e879");
-  main_layout->addWidget(label2, 0, 0, Qt::AlignRight | Qt::AlignTop);
   btn->setText(QObject::tr("Reboot"));
   QObject::connect(btn, &QPushButton::clicked, [=]() {
     Hardware::reboot();
