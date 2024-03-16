a2684603bea0f3d490543b32201ec2f2c311fbd2
crwusiz <crwusiz@gmail.com>
2024-03-16 21:57:41 +0900

    fix

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -118,7 +118,7 @@ def create_scc_commands(packer, accel, upper_jerk, idx, hud_control, set_speed, 
 
   scc11_values = {
     "MainMode_ACC": CS.out.cruiseState.available,
-    "TauGapSet": hud_control.leadDistanceBars + 1,
+    "TauGapSet": CS.out.cruiseState.gapAdjust,
     "VSetDis": set_speed if cruise_enabled else 0,
     "AliveCounterACC": idx % 0x10,
     "ObjValid": 1, # close lead makes controls tighter
