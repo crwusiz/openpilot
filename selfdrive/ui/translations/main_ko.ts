4f690d8663cd1eb144a4eec97391d6579855fb82
crwusiz <crwusiz@gmail.com>
2024-03-22 19:07:06 +0900

    update master

--- selfdrive/ui/translations/main_ko.ts
+++ selfdrive/ui/translations/main_ko.ts
@@ -477,6 +477,18 @@
         <translation>다시보기</translation>
     </message>
     <message>
+        <source>Pair your device with comma connect (connect.comma.ai) and claim your comma prime offer.</source>
+        <translation>장치를 comma connect (connect.comma.ai)에서 페어링하고 comma prime 무료 이용권을 사용하세요.</translation>
+    </message>
+    <message>
+        <source>Pair Device</source>
+        <translation>장치 페어링</translation>
+    </message>
+    <message>
+        <source>Pair</source>
+        <translation>페어링</translation>
+    </message>
+    <message>
         <source>Process</source>
         <translation>실행</translation>
     </message>
