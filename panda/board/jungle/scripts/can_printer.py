0306a7dc9ad28899b69a527c2a367591583aedfc
Vehicle Researcher <user@comma.ai>
2024-10-19 07:58:33 +0900

    master-ci

---
+++ panda/board/jungle/scripts/can_printer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import os
+import time
+from collections import defaultdict
+import binascii
+
+from panda import PandaJungle
+
+# fake
+def sec_since_boot():
+  return time.time()
+
+def can_printer():
+  p = PandaJungle()
+  print(f"Connected to: {p._serial}: {p.get_version()}")
+  time.sleep(1)
+
+  p.can_clear(0xFFFF)
+
+  start = sec_since_boot()
+  lp = sec_since_boot()
+  msgs = defaultdict(list)
+  canbus = os.getenv("CAN")
+  while True:
+    can_recv = p.can_recv()
+    for address,dat, src in can_recv:
+      if canbus is None or str(src) == canbus:
+        msgs[address].append((dat, src))
+
+    if sec_since_boot() - lp > 0.1:
+      dd = chr(27) + "[2J"
+      dd += "%5.2f\n" % (sec_since_boot() - start)
+      for k, v in sorted(msgs.items()):
+        last_msg, last_src = v[-1]
+        dd += "%d: %s(%6d): %s\n" % (last_src, "%04X(%4d)" % (k, k), len(v), binascii.hexlify(last_msg).decode())
+      print(dd)
+      lp = sec_since_boot()
+
+if __name__ == "__main__":
+  can_printer()
