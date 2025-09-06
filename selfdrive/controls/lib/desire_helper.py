7462779f66cddc6da1ce8874469031792f395d0b
crwusiz <crwusiz@gmail.com>
2025-09-06 21:34:34 +0900

    update master

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -48,6 +48,10 @@ class DesireHelper:
     self.auto_lane_change_timer = 0.0
     self.prev_torque_applied = False
 
+  @staticmethod
+  def get_lane_change_direction(CS):
+    return LaneChangeDirection.left if CS.leftBlinker else LaneChangeDirection.right
+
   def update(self, carstate, lateral_active, lane_change_prob):
     v_ego = carstate.vEgo
     one_blinker = carstate.leftBlinker != carstate.rightBlinker
@@ -61,14 +65,16 @@ class DesireHelper:
       if self.lane_change_state == LaneChangeState.off and one_blinker and not self.prev_one_blinker and not below_lane_change_speed:
         self.lane_change_state = LaneChangeState.preLaneChange
         self.lane_change_ll_prob = 1.0
+        # Initialize lane change direction to prevent UI alert flicker
+        self.lane_change_direction = self.get_lane_change_direction(carstate)
 
       # LaneChangeState.preLaneChange
       elif self.lane_change_state == LaneChangeState.preLaneChange:
         self.lane_change_pulse_timer += DT_MDL
-        # Set lane change direction
-        self.lane_change_direction = LaneChangeDirection.left if \
-          carstate.leftBlinker else LaneChangeDirection.right
-        torque_applied = carstate.steeringPressed and\
+        # Update lane change direction
+        self.lane_change_direction = self.get_lane_change_direction(carstate)
+
+        torque_applied = carstate.steeringPressed and \
                          ((carstate.steeringTorque > 0 and self.lane_change_direction == LaneChangeDirection.left) or
                           (carstate.steeringTorque < 0 and self.lane_change_direction == LaneChangeDirection.right)) or \
                          self.auto_lane_change_enable and (ALC_START_TIME + 0.25) > self.auto_lane_change_timer > ALC_START_TIME
