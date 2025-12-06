d3ccf4028829e025660e3335a8dfc4a251de18e9
crwusiz <crwusiz@gmail.com>
2025-12-07 08:10:43 +0900

    fix

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -753,7 +753,7 @@ class GuiApplication:
 
   @staticmethod
   def big_ui() -> bool:
-    return HARDWARE.get_device_type() in ('tici', 'tizi') or BIG_UI or PC
+    return HARDWARE.get_device_type() in ('tici', 'tizi') or BIG_UI
 
 
 gui_app = GuiApplication()
