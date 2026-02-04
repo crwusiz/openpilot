a1dbea36686ca6a88ce80ac6b27a0a1f53e40c8e
crwusiz <crwusiz@gmail.com>
2026-02-04 19:41:10 +0900

    update master

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -159,11 +159,12 @@ class LongitudinalPlanner:
     output_should_stop_e2e = sm['modelV2'].action.shouldStop
     output_v_target_e2e = sm['modelV2'].action.desiredVelocity
 
-    if (output_a_target_e2e < output_a_target_mpc) and sm['selfdriveState'].experimentalMode:
-      output_a_target = output_a_target_e2e
-      output_v_target = output_v_target_e2e
-      self.output_should_stop = output_should_stop_e2e
-      self.mpc.source = SOURCES[3]
+    if sm['selfdriveState'].experimentalMode:
+      output_a_target = min(output_a_target_e2e, output_a_target_mpc)
+      output_v_target = min(output_v_target_e2e, output_v_target_mpc)
+      self.output_should_stop = output_should_stop_e2e or output_should_stop_mpc
+      if output_a_target < output_a_target_mpc:
+        self.mpc.source = SOURCES[3]
     else:
       output_a_target = output_a_target_mpc
       output_v_target = output_v_target_mpc
