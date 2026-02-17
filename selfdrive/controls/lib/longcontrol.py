3369bb4839b234072818ea5269118323fd03d9b4
crwusiz <crwusiz@gmail.com>
2026-02-17 14:51:41 +0900

    fix

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -87,7 +87,7 @@ class LongControl:
         if self.prev_long_control_state == LongCtrlState.starting:
           output_accel -= self.CP.stoppingDecelRate * 1.5 * DT_CTRL
         else:
-          m_accel = -0.4
+          m_accel = -0.3
           d_accel = np.interp(output_accel,
                               [m_accel - 0.5, m_accel, m_accel + 0.5],
                               [self.CP.stoppingDecelRate, 0.05, self.CP.stoppingDecelRate])
@@ -113,7 +113,7 @@ class LongControl:
       error = long_plan.vTarget - CS.vEgo
       output_accel = self.pid.update(error, speed=CS.vEgo, feedforward=long_plan.aTarget)
 
-      self.stopping_accel_weight = max(self.stopping_accel_weight - 2. * DT_CTRL, 0.)
+      self.stopping_accel_weight = max(self.stopping_accel_weight - 1.0 * DT_CTRL, 0.)
       output_accel = self.last_output_accel * self.stopping_accel_weight + output_accel * (1. - self.stopping_accel_weight)
 
     self.last_output_accel = np.clip(output_accel, accel_limits[0], accel_limits[1])
--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -15,7 +15,7 @@ from openpilot.selfdrive.controls.lib.drive_helpers import CONTROL_N, get_accel_
 from openpilot.selfdrive.car.cruise import V_CRUISE_MAX, V_CRUISE_UNSET
 from openpilot.common.swaglog import cloudlog
 
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
+A_CRUISE_MAX_VALS = [1.2, 1.0, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.4
