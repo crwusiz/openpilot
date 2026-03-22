e275e0da60c2673733901df97bb4e5f760fffcaf
crwusiz <crwusiz@gmail.com>
2026-03-22 16:38:57 +0900

    fix

--- selfdrive/ui/mici/layouts/settings/settings.py
+++ selfdrive/ui/mici/layouts/settings/settings.py
@@ -41,7 +41,7 @@ class SettingsLayout(NavScroller):
     firehose_btn.set_click_callback(lambda: gui_app.push_widget(firehose_panel))
 
     community_panel = CommunityLayoutMici()
-    community_btn = SettingsBigButton("community", "", gui_app.texture("icons_mici/settings/community_icon.png"))
+    community_btn = SettingsBigButton("community", "", gui_app.texture("icons_mici/settings/community_icon.png", 52, 62))
     community_btn.set_click_callback(lambda: gui_app.push_widget(community_panel))
 
     self._scroller.add_widgets([
