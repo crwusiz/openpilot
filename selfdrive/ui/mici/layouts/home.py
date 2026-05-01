9a3005a5f491531dace0876fadf9e4d6b3b44c94
crwusiz <crwusiz@gmail.com>
2026-05-01 21:35:18 +0900

    update master

--- selfdrive/ui/mici/layouts/home.py
+++ selfdrive/ui/mici/layouts/home.py
@@ -22,7 +22,6 @@ from openpilot.system.ui.widgets.network import WifiManagerUI, WifiManager
 
 HEAD_BUTTON_FONT_SIZE = 40
 HOME_PADDING = 8
-SETTINGS_ZONE_WIDTH = 280
 ALERTS_ZONE_WIDTH = 180
 
 NetworkType = log.DeviceState.NetworkType
@@ -282,7 +281,6 @@ class MiciHomeLayout(Widget):
     if not self._did_long_press:
       relative_x = mouse_pos.x - self.rect.x
       has_alerts = self._alert_count_callback and self._alert_count_callback() > 0
-      #if relative_x < SETTINGS_ZONE_WIDTH:
       if rl.check_collision_point_rec(mouse_pos, self._settings_icon.rect):
         if self._on_settings_click:
           self._on_settings_click()
@@ -291,6 +289,8 @@ class MiciHomeLayout(Widget):
       elif has_alerts and relative_x > self.rect.width - ALERTS_ZONE_WIDTH:
         if self._on_alerts_click:
           self._on_alerts_click()
+      elif self._on_settings_click:
+        self._on_settings_click()
     self._did_long_press = False
 
   # --- Git & Commit Logic Methods ---
