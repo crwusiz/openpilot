35ceb117907ca2833184903734a3a315fd4968dc
crwusiz <crwusiz@gmail.com>
2026-07-15 08:47:03 +0900

    update master

--- openpilot/selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ openpilot/selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -289,14 +289,10 @@ class LongitudinalMpc:
     for i in range(N+1):
       self.solver.set(i, 'x', np.zeros(X_DIM))
     self.last_cloudlog_t = 0
-    self.status = False
     self.crash_cnt = 0.0
     self.solution_status = 0
     # timers
     self.solve_time = 0.0
-    self.time_qp_solution = 0.0
-    self.time_linearization = 0.0
-    self.time_integrator = 0.0
     self.x0 = np.zeros(X_DIM)
     self.set_weights()
 
@@ -374,7 +370,6 @@ class LongitudinalMpc:
     radarstate = sm['radarState']
     t_follow = get_T_FOLLOW(personality)
     v_ego = self.x0[1]
-    self.status = radarstate.leadOne.status or radarstate.leadTwo.status
 
     if radarstate.leadOne.status:
       j_lead = radarstate.leadOne.jLead
@@ -447,9 +442,6 @@ class LongitudinalMpc:
 
     self.solution_status = self.solver.solve()
     self.solve_time = float(self.solver.get_stats('time_tot')[0])
-    self.time_qp_solution = float(self.solver.get_stats('time_qp')[0])
-    self.time_linearization = float(self.solver.get_stats('time_lin')[0])
-    self.time_integrator = float(self.solver.get_stats('time_sim')[0])
 
     for i in range(N+1):
       self.x_sol[i] = self.solver.get(i, 'x')
