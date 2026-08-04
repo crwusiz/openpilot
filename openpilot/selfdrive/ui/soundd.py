7a4b60f9afeee9c0ab151ef75c66f0591ab29597
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-08-04 11:37:25 -0700

    agnos 19.2 (#38531)
    (cherry picked from commit 38ef0d2398546197f913aeabcccb64ed4542450b)

--- openpilot/selfdrive/ui/soundd.py
+++ openpilot/selfdrive/ui/soundd.py
@@ -170,6 +170,7 @@ class Soundd:
   def soundd_thread(self):
     # sounddevice must be imported after forking processes
     import sounddevice as sd
+    micd.patch_sounddevice(sd)
 
     sm = messaging.SubMaster(['selfdriveState', 'soundPressure'])
 
