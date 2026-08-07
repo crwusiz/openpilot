b7e4e1a2d423da3889adbd763b4f3440d232b025
crwusiz <crwusiz@gmail.com>
2026-08-07 19:50:07 +0900

    fix

--- openpilot/system/camerad/sensors/sensor.h
+++ openpilot/system/camerad/sensors/sensor.h
@@ -10,6 +10,7 @@
 #include "media/cam_sensor.h"
 
 #include "openpilot/cereal/gen/cpp/log.capnp.h"
+#include "system/camerad/sensors/ar0231_registers.h"
 #include "system/camerad/sensors/ox03c10_registers.h"
 #include "system/camerad/sensors/os04c10_registers.h"
 
@@ -87,6 +88,17 @@ public:
   };
 };
 
+class AR0231 : public SensorInfo {
+public:
+  AR0231();
+  std::vector<i2c_random_wr_payload> getExposureRegisters(int exposure_time, int new_exp_g, bool dc_gain_enabled) const override;
+  float getExposureScore(float desired_ev, int exp_t, int exp_g_idx, float exp_gain, int gain_idx) const override;
+  int getSlaveAddress(int port) const override;
+
+private:
+  mutable std::map<uint16_t, std::pair<int, int>> ar0231_register_lut;
+};
+
 class OX03C10 : public SensorInfo {
 public:
   OX03C10();
@@ -98,6 +110,7 @@ public:
 class OS04C10 : public SensorInfo {
 public:
   OS04C10();
+  void ife_downscale_configure();
   std::vector<i2c_random_wr_payload> getExposureRegisters(int exposure_time, int new_exp_g, bool dc_gain_enabled) const override;
   float getExposureScore(float desired_ev, int exp_t, int exp_g_idx, float exp_gain, int gain_idx) const override;
   int getSlaveAddress(int port) const override;
